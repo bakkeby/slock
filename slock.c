dc5bc84a275f8a5b643331436603ad810bd4fda4
bakkeby <bakkeby@gmail.com>
2026-08-17 07:50:27 +0200

    Bump to b04f4d0.
    bump version to 1.7
    
    https://git.suckless.org/slock/commit/b04f4d0810f0c54a64312da39a85be2b372aa80a.html
    
    Avoid ctype(3) abuses
    
    Valid argument of ctype(3) functions must be either EOF or non-negative
    value within the range representable as unsigned char. Invalid values
    leads to undefined behavior.
    
    Cast all such uses to avoid that.
    
    At least in NetBSD 11 ctype(3) abuses will always crash and permit
    to break slock screen locking.
    
    https://git.suckless.org/slock/commit/55d985a354048e14f408dd731faaf1acbdc277f4.html

--- slock.c
+++ slock.c
@@ -397,7 +397,7 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens, const
 				if (enabled(ControlClear) && iscntrl((int)buf[0]) && !num)
 					continue;
 
-				if (num && !iscntrl((int)buf[0]) &&
+				if (num && !iscntrl((unsigned char)buf[0]) &&
 					(len + num < sizeof(passwd)))
 				{
 					memcpy(passwd + len, buf, num);
